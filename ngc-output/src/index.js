505b90e6c12a23f7ec52726d2c900d581b45a80d
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-10-24 22:27:26 +0100

    Remove ngc output

--- ngc-output/src/index.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-import 'rxjs/Observable';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=index.js.map
